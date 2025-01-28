beacc46dac28802a97528ee63cefd4e28e5c6349
CannataJulian <cannatajulian@gmail.com>
2025-01-28 19:51:43 -0300

    v1.0

---
+++ JS/carousel.js
@@ -0,0 +1,21 @@
+let currentIndex = 0;
+
+function showSlide(index) {
+  const slides = document.querySelectorAll('.slide');
+
+  if (index >= slides.length) {
+    currentIndex = 0;
+  } else if (index < 0) {
+    currentIndex = slides.length - 1;
+  } else {
+    currentIndex = index;
+  }
+  const offset = -currentIndex * 100;
+  document.querySelector('.slides').style.transform = `translateX(${offset}%)`;
+}
+
+function changeSlide(direction) {
+  showSlide(currentIndex + direction);
+}
+
+showSlide(currentIndex);
